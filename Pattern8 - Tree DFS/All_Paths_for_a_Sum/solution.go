bf081ba959df40521a31a90df4f3c01d684b714f
fengzw <zhiwei.feng1995@gmail.com>
2021-04-22 22:05:55 +0800

    update All_Paths_for_a_Sum 代码的注释

--- Pattern8 - Tree DFS/All_Paths_for_a_Sum/solution.go
+++ Pattern8 - Tree DFS/All_Paths_for_a_Sum/solution.go
@@ -24,10 +24,10 @@ func pathSum(root *TreeNode, targetSum int) (ans [][]int) {
 		if left == 0 && root.Left == nil && root.Right == nil {
 			// 解释下为什么要这么做，而不是
 			// ans = append(ans, path)
-			// 因为如果像上面做，其实在ans存放的是path指针所指向的地址区，因此path的变化会影响到ans的内容
+			// 因为如果像上面做，其实在ans存放的是path这个slice，而path的变化会影响到ans的内容
 			// 而我们想要的是当ans append操作的时候，是加入一个immutable的[]int，
 			// 因此必须使用append([]int{}, path...)重新分配一块新的内存空间存放path的复制体，并用一个新的指针指向
-			// 而此时ans保存的是新的指针指向的地址区，而且不会在随后的代码执行中被修改。
+			// 而此时ans保存的是复制体，则不会在随后的代码执行中被修改。
 			ans = append(ans, append([]int{}, path...))
 			return
 		}
