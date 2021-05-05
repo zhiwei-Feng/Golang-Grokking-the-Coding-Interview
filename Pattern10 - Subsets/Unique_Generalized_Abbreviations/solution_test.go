1e1b90012b88d57b999ec3971d702918939399b9
fengzw <zhiwei.feng1995@gmail.com>
2021-05-05 16:35:11 +0800

    add: my solution for Unique_Generalized_Abbreviations

--- Pattern10 - Subsets/Unique_Generalized_Abbreviations/solution_test.go
+++ Pattern10 - Subsets/Unique_Generalized_Abbreviations/solution_test.go
@@ -1,26 +1,26 @@
-package Balanced_Parentheses
+package Unique_Generalized_Abbreviations
 
 import (
 	"testing"
 )
 
-func Test_generateParenthesis(t *testing.T) {
+func Test_generateAbbreviations(t *testing.T) {
 	type args struct {
-		n int
+		word string
 	}
 	tests := []struct {
 		name string
 		args args
 		want []string
 	}{
-		{"case1", args{3}, []string{"((()))", "(()())", "(())()", "()(())", "()()()"}},
-		{"case2", args{1}, []string{"()"}},
-		{"case3", args{4}, []string{"(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"}},
-		{"case4", args{0}, []string{}},
+		{"case1", args{"BAT"}, []string{"BAT", "BA1", "B1T", "B2", "1AT", "1A1", "2T", "3"}},
+		{"case2", args{"code"}, []string{"code", "cod1", "co1e", "co2", "c1de", "c1d1", "c2e", "c3", "1ode", "1od1", "1o1e", "1o2",
+			"2de", "2d1", "3e", "4"}},
+		{"case3", args{""}, []string{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := generateParenthesis(tt.args.n)
+			got := generateAbbreviations(tt.args.word)
 			want := tt.want
 			if len(got) != len(want) {
 				t.Errorf("generateParenthesis() = %v, want %v", got, tt.want)
