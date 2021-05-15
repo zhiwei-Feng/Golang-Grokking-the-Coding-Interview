8fbf6f5718124684ca8d7cce7acb14990e21649e
fengzw <zhiwei.feng1995@gmail.com>
2021-05-15 23:44:24 +0800

    update: append more test case for challenge2searchinarotatedarray

--- Pattern11 - Modified Binary Search/Challenge2-Search_in_a_Rotated_Array/solution_test.go
+++ Pattern11 - Modified Binary Search/Challenge2-Search_in_a_Rotated_Array/solution_test.go
@@ -15,6 +15,8 @@ func Test_search(t *testing.T) {
 		{"case1", args{[]int{4, 5, 6, 7, 0, 1, 2}, 0}, 4},
 		{"case2", args{[]int{4, 5, 6, 7, 0, 1, 2}, 3}, -1},
 		{"case3", args{[]int{1}, 0}, -1},
+		{"case4", args{[]int{10, 15, 1, 3, 8}, 15}, 1},
+		{"case5", args{[]int{4, 5, 7, 9, 10, -1, 2}, 10}, 4},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
