19757306fdfdd7ad863806f3b3d98d2e2b65a89d
fengzw <zhiwei.feng1995@gmail.com>
2021-06-16 23:59:49 +0800

    add: complete all corner testcase for pattern16 challenge1

--- Pattern16 - Topological Sort/Challenge1/solution_test.go
+++ Pattern16 - Topological Sort/Challenge1/solution_test.go
@@ -17,6 +17,9 @@ func Test_sequenceReconstruction(t *testing.T) {
 		{"case3", args{[]int{1, 2, 3, 4}, [][]int{{1, 2}, {2, 3}, {3, 4}}}, true},
 		{"case4", args{[]int{1, 2, 3, 4}, [][]int{{1, 2}, {2, 3}, {2, 4}}}, false},
 		{"case5", args{[]int{3, 1, 4, 2, 5}, [][]int{{3, 1, 5}, {1, 4, 2, 5}}}, true},
+		{"case6", args{[]int{}, [][]int{{3, 1, 5}, {1, 4, 2, 5}}}, false},
+		{"case7", args{[]int{1, 2, 3, 4}, [][]int{{1, 2}, {1, 3}, {2, 3}, {4, 2}}}, false},
+		{"case8", args{[]int{1, 2, 3, 4}, [][]int{{1, 3}, {3, 4}, {4, 2}}}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
