f695a954bd00405c5c17916f3e6be7b54939b755
fengzw <zhiwei.feng1995@gmail.com>
2021-06-23 23:33:52 +0800

    add: quick sort partition solution for Miscellaneous

---
+++ Miscellaneous/quick-sort.go
@@ -0,0 +1,40 @@
+package Miscellaneous
+
+//
+// time complexity: O(klogn + n)
+// space complexity: O(n)
+//
+func findKthSmallestNumberUsingQuickSort(nums []int, k int) int {
+	if k > len(nums) {
+		return -1
+	}
+	return partitionRec(nums, k, 0, len(nums)-1)
+}
+
+func partitionRec(nums []int, k, start, end int) int {
+	p := partition(nums, start, end)
+	if p == k-1 {
+		return nums[p]
+	}
+	if p > k-1 {
+		return partitionRec(nums, k, start, p-1)
+	}
+	return partitionRec(nums, k, p+1, end)
+}
+
+func partition(nums []int, low, high int) int {
+	if low == high {
+		return low
+	}
+
+	pivot := nums[high]
+	i := low - 1
+	for j := low; j < high; j++ {
+		if nums[j] < pivot {
+			i++
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	nums[i+1], nums[high] = nums[high], nums[i+1]
+	return i + 1
+}
